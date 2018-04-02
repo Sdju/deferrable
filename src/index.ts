3a9dde2a5cef584baff2e02f7ff64aebbb81d383
Denis Chernov <zede@ventumit.ru>
2018-04-02 22:56:14 +0500

    add implementation of deferable

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export function deferable(f: Function): Function {
+	return (...args: any[]) => new Promise((resolve, reject)=> {
+		const defers: Function[] = [];
+		const defer = (f: Function) => {
+			defers.push(f);
+		};
+		const deferHandler = async (f: Function): void => {await f()};
+		try {
+			let res: any = f(defer, ...args);
+			if (res instanceof Promise) {
+				res.then((val) => {
+					Promise.all(defers.map(deferHandler)).then(() => {
+						resolve(val);
+					}).catch((err) => {
+						reject(err);
+					});
+				}).catch((err) => {
+					reject(err);
+				});
+			} else {
+				Promise.all(defers.map(deferHandler)).then(() => {
+					resolve(res);
+				}).catch((err) => {
+					reject(err);
+				});
+			}
+		} catch (err) {
+			reject(err);
+		}
+	});
+}
